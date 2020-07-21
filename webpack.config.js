10654613ba83b4d1ca16974c4512e10c64830482
MiguelDP4 <migueldp4@gmail.com>
2020-07-21 14:03:32 -0600

    change style.css file position to make style recognition easier

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,19 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          }
+        ]
+      }
     ],
   },
 
